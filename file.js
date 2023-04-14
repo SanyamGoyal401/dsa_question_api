fbd81af51f885a0458ebc2766682f3e7b0c851e9
Sanyam <sanyamgoyal2859@gmail.com>
2023-04-14 11:16:40 +0530

    added topic api

--- file.js
+++ file.js
@@ -9,6 +9,25 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send(data);
 })
+
+app.get('/topic', (req, res)=>{
+    let set = new Set();
+    data.forEach((object)=>{
+        set.add(object.Topic);
+    })
+    let topics = [...set];
+    let topicData = [];
+    topics.forEach((topic)=>{
+        let numberOfQuestions = data.filter((object)=>{
+            return object.Topic === topic;
+        })
+        topicData.push({
+            topic: topic,
+            numberOfQuestions: numberOfQuestions.length
+        })
+    })
+    res.send(topicData);
+})
 app.get('/:topic', (req, res)=>{
     const topic = req.params.topic;
     let response = data.filter((object)=>{
